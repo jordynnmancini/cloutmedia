003f1889cfdc018c87280bc3a1173c5b3cce0518
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-06-22 21:07:56 -0400

    split user model into artist and engineer models

---
+++ models/engineer.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// hashing password? 
+// what other details do we need specific to engineers? 
+
+const engineerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    preferredName: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        data: Buffer,
+        contentType: String,
+        required: false 
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, 'is invalid'],
+    },
+    bio: {
+        type: String,
+        required: false
+    },
+    primaryLocation: {
+        type: String,
+        required: true
+    }
+},
+    { timestamps: true }
+);
+
+const Engineer = mongoose.model("Engineer", engineerSchema);
+
+module.exports = engineerSchema;
