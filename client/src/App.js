6c5ed4a38b1f77a2263c1014e5747f19fe75f40c
Andre Rodriguez <drae7299@gmail.com>
2021-06-22 23:42:29 -0400

    made inititial home screen, topbar/navbar and side menu with dynamic scss sass. Will be adding in back drop photos and wrapping the topbar and menu components into one component to simplidy app .js and to assist with image overlay etc.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
-
+import Topbar from "./components/exampleComponent/topbar/Topbar"
+import Menu from "./components/exampleComponent/menu/Menu"
 function App() {
 
   // useState, useEffect, etc. 
+  const [menuOpen, setMenuOpen] = useState(true)
 
   //return code here
   return (<>
-  
+   <div className="app">
+      body component and add in the other page components into it ie. the image etc(possible modals)
+
+      <Topbar menuOpen={menuOpen}  setMenuOpen={setMenuOpen}/>
+     <Menu menuOpen={menuOpen}  setMenuOpen={setMenuOpen}/>
+      <div>
+       //insert sections for the page
+     </div>    
+    </div>
   </>);
 }
 
