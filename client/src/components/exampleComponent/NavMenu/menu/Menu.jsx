88d4944219a1c80658a14a2b53217fb6cbaabe85
Andre Rodriguez <drae7299@gmail.com>
2021-06-24 19:05:04 -0400

    redid menu dynamic animation

--- client/src/components/exampleComponent/NavMenu/menu/Menu.jsx
+++ client/src/components/exampleComponent/NavMenu/menu/Menu.jsx
@@ -1,19 +1,19 @@
 import "./menu.scss"
 
-export default function Menu({ navMenuOpen, setNavMenuOpen }) {
+export default function Menu({ menuOpen, setMenuOpen }) {
     return (
-        <div className={"menu " + (navMenuOpen && "active")}>
+        <div className={"menu " + (menuOpen && "active")}>
             <ul>
-                <li onClick={() => setNavMenuOpen(!navMenuOpen)}>
+                <li onClick={() => setMenuOpen(!menuOpen)}>
                     <a href="#intro">Home</a>
                 </li>
-                <li onClick={() => setNavMenuOpen(!navMenuOpen)}>
+                <li onClick={() => setMenuOpen(!menuOpen)}>
                     <a href="#portfolio">Portfolio</a>
                 </li>
-                <li onClick={() => setNavMenuOpen(!navMenuOpen)}>
+                <li onClick={() => setMenuOpen(!menuOpen)}>
                     <a href="#works">Works</a>
                 </li>
-                <li onClick={() => setNavMenuOpen(!navMenuOpen)}>
+                <li onClick={() => setMenuOpen(!menuOpen)}>
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
