810ac8816c1543cb1a84544f0e64b0a9f589a68b
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-06-24 21:13:32 -0400

    more in login.js

--- client/src/login.js
+++ client/src/login.js
@@ -1,3 +1,19 @@
+import React, { Component } from "react";
 import AuthHelperMethods from "./components/AuthHelperMethods";
+import { Link } from "react-router-dom"; 
 
-Auth = new AuthHelperMethods(); 
+class Login extends Component {
+    Auth = new AuthHelperMethods(); 
+    
+    state = {
+        username: "",
+        password: ""
+    }
+
+    handleFormSubmit = e => {
+        e.preventDefault();
+
+        this.Auth.login(this.state.username)
+
+    }
+}
