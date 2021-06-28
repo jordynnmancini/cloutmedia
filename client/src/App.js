4fa39671d8e751184e0d793c7491ce709db7edc6
Andre Rodriguez <drae7299@gmail.com>
2021-06-28 17:16:37 -0400

    wrapped app js in a router from react router to handle all switch cases for user and also check with Auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
 import React, { useState, useEffect} from "react";
 import Home from "./components/pages/Home/Home";
 import NavMenu from "./components/NavMenu/NavMenu";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import withAuth from "./components/higher-order-component/withAuth";
+import Dashboard from "../src/components/pages/Dashboard"
+
+
 function App() {
 
   // useState, useEffect, etc. 
@@ -9,14 +21,24 @@ function App() {
   const toggleMenu = () => setMenuOpen(!menuOpen)
 
   //return code here
-  return (<>
-   <div className="app">
+  return (
+    <Router>
+     <div className="app">
       <NavMenu menuOpen={menuOpen} toggleMenu={toggleMenu} />
       <div className="sections">
       <Home />
      </div>    
     </div>
-  </>);
+        <Switch>
+        <Route path="/dashboard" component={withAuth(Dashboard)} />
+        <Route path="/discover" component={withAuth(Dashboard)} />
+        <Route path="/dashboard" component={withAuth(Dashboard)} />
+        <Route path="/dashboard" component={withAuth(Dashboard)} />
+        </Switch>
+    </Router>
+  );
 }
 
-export default App;
+
+
+  export default App;
