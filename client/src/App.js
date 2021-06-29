d37874c008a00b59b54662099dbcd6e6e5c786fb
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-06-29 18:54:01 -0400

    updated import labels that were causing it to break

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Home from "./components/pages/Home/Home";
 import NavMenu from "./components/NavMenu/NavMenu";
-import Signup from "../src/components/Signup/Signup";
+import Signup from "../src/components/Signup/signup";
 import withAuth from "./components/higher-order-component/withAuth";
 import Dashboard from "../src/components/pages/Dashboard";
 import Discover from "../src/components/pages/Discover/Discover";
 import About from "../src/components/pages/About/About";
 import Contact from "../src/components/pages/Contact/Contact";
-import Login from "../src/components/Login/Login" 
+import Login from "../src/components/Login/login"; 
 import {
   BrowserRouter as Router,
   Switch,
