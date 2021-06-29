75aca24e126d6ffc3f7e9150b7e1cace5756d41c
Andre Rodriguez <drae7299@gmail.com>
2021-06-29 13:39:49 -0400

    added in scss files for remaining components and pages

--- client/src/components/Signup/signup.js
+++ client/src/components/Signup/signup.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import API from "../../utils/API";
-import "./signup.css";
+import "./signup.scss";
+
 
 export default class Signup extends Component {
     constructor(props) {
