c270e1c3db0af9bf577df026ece86d52cd57ac35
Andre Rodriguez <drae7299@gmail.com>
2021-06-28 23:23:46 -0400

    refactored code

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,29 +1,19 @@
-import React from 'react';
+import React from "react";
 import "./topbar.scss";
-import EmailIcon from "@material-ui/icons/Email";
 import PersonIcon from "@material-ui/icons/Person";
 
-
 export default function Topbar({ menuOpen, toggleMenu }) {
-  
-  
   return (
     <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
-        <div className="left">
-          <a className="logo">
-            Clout
-          </a>
-          <div className="itemContainer">
-            <PersonIcon fontSize="large" className="icon" />
-            <span></span>
+          <div className="left" className="logo"> Clout
+            <div className="itemContainer">
+              <PersonIcon fontSize="large" className="icon" />
+              <span></span>
+            </div>
           </div>
-          <div className="itemContainer">
-            <span></span>
-          </div>
-        </div>
-        <div className="right">
-          <div className= "hamburger" onClick={toggleMenu}>
+          <div className="right">
+          <div className="hamburger" onClick={toggleMenu}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
