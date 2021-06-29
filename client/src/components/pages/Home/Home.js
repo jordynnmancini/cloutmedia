45cf51eef68d25d05ffe698e54a41b3559d81904
Andre Rodriguez <drae7299@gmail.com>
2021-06-28 23:31:31 -0400

    attemped to make modal for login, have a better idea to approach it tomorrow night

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import { Modal } from "@material-ui/core";
+import React from "react";
 import "./home.scss";
 
 export default function Home() {
-    return (
-        <div className="home">
-        <div className="container">
-           <div className="imgContainer">
-                <img src="assets/logo.png" alt="logo for clout media - connecting the music industry together"/>
-           </div>
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="imgContainer">
+          <img
+            src="assets/logo.png"
+            alt="logo for clout media - connecting the music industry together"
+          />
         </div>
-        <div className="buttons">
-        <button className="signin" type="submit"> Signup!</button>
-        <button className="learnMore" type="submit">Learn More!</button>
-        </div>
-        
-        </div>
-    )
+      </div>
+      <div className="buttons">
+        <button className="signup" type="submit">
+          Signup!
+        </button>
+        <button className="learnMore" type="submit">
+          Learn More!
+        </button>
+      </div>
+    </div>
+  );
 }
-
-
-
