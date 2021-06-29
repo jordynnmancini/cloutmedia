235b7d1c421961c074574197cd085ecf8ce785e1
Andre Rodriguez <drae7299@gmail.com>
2021-06-29 15:08:51 -0400

    starting to work on login scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Home from "./components/pages/Home/Home";
 import NavMenu from "./components/NavMenu/NavMenu";
-import Signup from "./components/Signup/Signup";
+import Signup from "../src/components/Signup/Signup";
+import withAuth from "./components/higher-order-component/withAuth";
+import Dashboard from "../src/components/pages/Dashboard";
+import Discover from "../src/components/pages/Discover/Discover";
+import About from "../src/components/pages/About/About";
+import Contact from "../src/components/pages/Contact/Contact";
+import Login from "../src/components/Login/Login" 
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,12 +16,7 @@ import {
   useRouteMatch,
   useParams,
 } from "react-router-dom";
-import withAuth from "./components/higher-order-component/withAuth";
-import Dashboard from "../src/components/pages/Dashboard";
-import Discover from "../src/components/pages/Discover/Discover";
-import About from "../src/components/pages/About/About";
-import Contact from "../src/components/pages/Contact/Contact";
-import Login from "../src/components/Login/Login" ;
+;
   
 function App() {
   //replaced this from jordyn's code, it may be needed for with
