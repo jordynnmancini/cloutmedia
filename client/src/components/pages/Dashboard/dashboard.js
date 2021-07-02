c58faf3cd8a61df055e16fa216b24312ec12fcfd
Andre Rodriguez <drae7299@gmail.com>
2021-07-01 20:35:27 -0400

    made scss for dashboard page and will begin styling now

--- client/src/components/pages/Dashboard/dashboard.js
+++ client/src/components/pages/Dashboard/dashboard.js
@@ -4,6 +4,28 @@ import "./dashboard.scss";
 export default function Dashboard() {
   return (
     <div className="dashboard">
+      <div className="container">
+        <h2> My Dashboard</h2>
+        <div className="left">
+          <div className="info">
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+        <div className="right">
+          <div className="profileEdit">
+            <button className="profileButton" type="submit"></button>
+            <button className="previewButton" type="submit"></button>
+            <button className="settingsButton" type="submit"></button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
