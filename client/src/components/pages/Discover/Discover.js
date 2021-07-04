43ce8b29e4f93ac3cb91e1f52feebfa99d8399be
Andre Rodriguez <drae7299@gmail.com>
2021-07-03 21:37:00 -0400

    created main frame and div sections for discovery page, will continue to add in rows for the artist or engineer info to append too

--- client/src/components/pages/Discover/Discover.js
+++ client/src/components/pages/Discover/Discover.js
@@ -1,8 +1,29 @@
 import React from "react";
-import "./discovery.scss"
+import "./discovery.scss";
 
-export default function Discovery(){
-    return(
-        <h3>Testing</h3>
-    )
+export default function Discovery() {
+  return (
+    <div className="discoverWrapper">
+      <div className="discoverBanner">
+        <h3>Discover</h3>
+      </div>
+      <div className="content">
+        <div className="top">
+          <div className="gpsBox"></div>
+        </div>
+        <div className="bottom">
+          <div className="left">
+              <h3>Search By Location</h3>
+              <search className="inputSearch">
+              <input type="search" placeholder="Search" results="0" />
+              <button className="searchButton" type="submit"></button>
+              </search>
+          </div>
+          <div className="right">
+              <h3>Sound Engineers in Your Area</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
