df4e64694b15d1577276729cc87c800149456c39
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-07-05 16:41:41 -0400

    styling needs some work, but functionality is mostly there

--- client/src/components/pages/Discover/Discover.js
+++ client/src/components/pages/Discover/Discover.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect, useRef } from "react";
 import "./discovery.scss";
 import { init } from "ityped";
 import SearchIcon from '@material-ui/icons/Search';
-import API from '../../../utils/API'; 
+import API from '../../../utils/API';
 
 export default function Discovery() {
   const [results, setResults] = useState([]);
   const [type, setType] = useState();
-  const [location, setLocation] = useState(); 
+  const [location, setLocation] = useState();
 
   const textRef = useRef();
   useEffect(() => {
@@ -19,17 +19,21 @@ export default function Discovery() {
     });
   }, []);
 
-  const handleInputChange = e => {
+  const handleTypeChange = e => {
     const { value } = e.target;
     setType(value);
-    setLocation(value); 
+  };
+
+  const handleLocationChange = e => {
+    const { value } = e.target;
+    setLocation(value);
   }
 
   const handleSearchSubmit = e => {
-    e.preventDefault(); 
+    e.preventDefault();
     API.getSearchResults(type, location)
       .then(res => setResults(res.data))
-      .catch(err => console.log(err)); 
+      .catch(err => console.log(err));
   }
 
   return (
@@ -38,40 +42,49 @@ export default function Discovery() {
         <h3 ref={textRef}></h3>
       </div>
       <div className="content">
-        <div className="top">
-          <div className="gpsBox"></div>
+        <div className='top'>
+          <div className='left'>
+            <h3>Search:</h3>
+            <label>Discover:</label>
+            <select value={type} onChange={handleTypeChange}>
+              <option value="Artist">Artists/Musicians</option>
+              <option value="Sound Engineer">Sound Engineers</option>
+            </select>
+            <label>In:</label>
+            <select value={location} onChange={handleLocationChange}>
+              <option>Nashville</option>
+              <option>Los Angeles</option>
+              <option>New York City</option>
+            </select>
+            <button onClick={handleSearchSubmit} className="searchButton" type="submit" ><SearchIcon fontSize="small" /></button>
+          </div>
         </div>
         <div className="bottom">
-          <div className="left">
-            <h3>Search By Location:</h3>
-            <search className="inputSearch">
-              <input type="search" placeholder="Search" results="0" />
-              <button className="searchButton" type="submit" ><SearchIcon fontSize="small"/></button>
-            </search>
-          </div>
           <div className="right">
-            <h3>Sound Engineers in Your Area:</h3>
-          <div className="soundEngineers">
-          <div className="uls">
-            <ul className="eng1">
-              <li className="name">John Doe</li>
-              <li className="title">Engineer</li>
-              <a href="./whoopsie"><li className="info">More Info</li></a>
-            </ul>
-            <ul className="eng2">
-            <li className="name">John Doe</li>
-              <li className="title">Engineer</li>
-              <a href="./whoopsie"><li className="info">More Info</li></a>
-            </ul>
-            <ul className="eng3">
-            <li className="name">John Doe</li>
-              <li className="title">Engineer</li>
-              <a href="./whoopsie"><li className="info">More Info</li></a>
-            </ul>
+            <h3>Results in Your Area:</h3>
+            <div className="soundEngineers">
+              <div className="uls">
+                {!results.length ? (
+                  <h1>no results</h1>
+                ) : (
+                  <div>
+                    {results.map(result => {
+                      return (
+                        <ul className="eng1">
+                          <li className="name">{result.name}</li>
+                          <li className="title">{result.type}</li>
+                          <a href="./whoopsie"><li className="info">More Info</li></a>
+                        </ul>
+                      )
+                    })}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-          </div>
         </div>
+
+
       </div>
     </div>
   );
