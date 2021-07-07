4370f5648331346231225000cf47a18bf78c12cb
Sarehon Taylor <74032335+SarehonTaylor@users.noreply.github.com>
2021-07-06 23:48:15 -0400

    made changes to About Us and Contact Pages Using Material Design, also installed some other npm packages for icons feel free to delete those

--- client/src/components/pages/Contact/Contact.js
+++ client/src/components/pages/Contact/Contact.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import "./contact.scss"
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import PeopleIcon from '@material-ui/icons/People';
+
 
 export default function Contact() {
     return (
         <div className="contactWrapper">
         <div className="contactUs">
-            
-        </div>
-        </div>
-    )
+        <img className="cloutlogo"
+            src="assets/logo.png"
+            alt="logo for clout media - FOR MUSIC INFLUENCERS ONLY." />
+    </div>   
+     <div className="main">
+
+        <h3>Hello If You Have A Question Or An Improvement You Think Would Be A Great Addition To Clout Drop Us A Line Below!</h3>
+        <div className="socialLogos">
+        <MailOutlineIcon fontSize ="large"/>
+        <GitHubIcon fontSize ="large"/>
+        <PeopleIcon fontSize ="large"/>
+        <h4><li><a href="https://github.com/drae7299"> Andre Rodriguez</a></li></h4> 
+        <h4><li><a href="https://github.com/jordynnmancini" > Jordyn Mancini</a></li></h4>
+        <h4><li><a href="https://github.com/oconnor97"> Michael O'Connor</a></li></h4>
+        <h4><li><a href="https://github.com/SarehonTaylor" target="_blank"> Sarehon Taylor</a></li></h4>
+        
+     </div>
+    </div>
+    </div>
+  );
 }
+        
+
+
