ecbda3ba93683648af14d0cafeefeb7d8c72ca9f
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-07-07 20:38:52 -0400

    dashboard

--- client/src/components/pages/Dashboard/dashboard.js
+++ client/src/components/pages/Dashboard/dashboard.js
@@ -3,20 +3,28 @@ import "./dashboard.scss";
 import { init } from "ityped";
 import { useEffect, useRef, useState } from "react";
 import API from "../../../utils/API";
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 export default function Dashboard() {
-
-  // const id = localStorage.getItem('jwtToken').split('/')[0]
+  const id = localStorage.getItem('jwtToken').split('/')[0]
   const [userData, setUserData] = useState({});
   const [primaryLocation, setPrimaryLocation] = useState();
   const [subType, setSubType] = useState();
   const [phoneNumber, setPhoneNumber] = useState();
   const [bio, setBio] = useState();
-
-
-
-
-
+  const [modalIsOpen, setIsOpen] = useState(false);
+  
   const textRef = useRef();
   useEffect(() => {
     init(textRef.current, {
@@ -27,15 +35,12 @@ export default function Dashboard() {
       backSpeed: 60,
       showCursor: true,
     });
-
   }, []);
 
-
   useEffect(() => {
-    const id = localStorage.getItem('jwtToken').split('/')[0]
+    // const id = localStorage.getItem('jwtToken').split('/')[0]
     console.log(id, 'id')
     API.getUserData(id)
-
       .then(res => {
         setUserData(res.data)
         console.log(res.data)
@@ -66,15 +71,25 @@ export default function Dashboard() {
 
   const handleUpdateSubmit = (e) => {
     e.preventDefault();
-    API.updateUserData({
+    setIsOpen(false);
+
+    API.updateUserData(id, {
       bio: bio,
       subType: subType,
       phoneNumber: phoneNumber,
       primaryLocation: primaryLocation,
-
     })
+      .then(res => setUserData(res.data))
+      .catch(err => console.log(err))
   }
 
+  const openModal = () => {
+    setIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsOpen(false);
+  }
 
   return (
     <div className="dashboard">
@@ -91,32 +106,75 @@ export default function Dashboard() {
           <div className="bottom">
             <div className="info">
               <ul>
-                <br />
+              <br />
                 <li>{userData.name}</li>
                 <li>{userData.type}</li>
                 <li>{userData.primaryLocation}</li>
                 <li>{userData.email}</li>
-                <li>Cell: 614-592-8914</li>
+                <li>{userData.phoneNumber}</li>
                 <br />
-                <li>About Me!</li>
-                <p>
-                  "Sed ut perspiciatis unde omnis iste natus error sit
-                  voluptatem accusantium doloremque laudantium, totam rem
-                  aperiam, eaque ipsa quae ab illo inventore veritatis et quasi kjhdskjhds kjhdsakjhd kjhdaskjhdas
-
-                </p>
+                <li>About Me:</li>
+                {!userData.bio ? (
+                  <p>edit your profile to include a bio. Increase your clout by showing off your accomplishments. </p>
+                ) : (
+                  <p>
+                    {userData.bio}
+                  </p>
+                )}
               </ul>
             </div>
           </div>
         </div>
         <div className="right">
           <div className="profileEdit">
-            <button className="profileButton" type="submit">Edit Your Profile</button>
+            <button onClick={openModal} className="profileButton" type="submit">Edit Your Profile</button>
             <button className="previewButton" type="submit">Preview</button>
             <button className="settingsButton" type="submit">Setting/Security</button>
           </div>
         </div>
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="update profile form"
+      >
+        <input
+          placeholder="prefer to be contacted by phone? Enter your number here"
+          name="phoneNumber"
+          type="text"
+          value={phoneNumber}
+          onChange={handlePhoneNumberChange}
+        />
+        <input
+          placeholder="enter more about your profession(i.e. singer, guitar player, etc)"
+          name="subType"
+          type="text"
+          value={subType}
+          onChange={handleSubTypeChange}
+        />
+        <input
+          placeholder="enter a bio"
+          name="bio"
+          type="text"
+          value={bio}
+          onChange={handleBioChange}
+        />
+        <label className="labelFor" for="primaryLocation"> Moving? Update your location: </label>
+        <select
+          value={primaryLocation}
+          name="primaryLocation"
+          onChange={handlePrimaryLocationChange}>
+          <option value="Nashville">Nashville, TN</option>
+          <option value="Los Angeles">Los Angeles, CA</option>
+          <option value="New York City">New York City, NY</option>
+        </select>
+
+        <button onClick={handleUpdateSubmit}> save & close</button>
+        <button onClick={closeModal}>close without saving</button>
+      </Modal>
+
     </div>
   );
 }
